924692c2f021f2af28d556ec40bc6fce6f87ba60
Sneha_kumari <54633208+s266@users.noreply.github.com>
2025-04-08 23:11:18 +0530

    Add files via upload

---
+++ checkout/manual.php
@@ -0,0 +1,50 @@
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+<button id="rzp-button1" class="btn btn-primary btn-block">PAY NOW</button>
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<form name='razorpayform' action="../razorpay/verify.php" method="POST">
+    <input type="hidden" name="razorpay_payment_id" id="razorpay_payment_id">
+    <input type="hidden" name="razorpay_signature"  id="razorpay_signature" >
+    <input type="hidden" name="razorpay_order_id"  id="razorpay_order_id" value="<?php echo $razorpayOrderId; ?>">
+</form>
+<script>
+// Checkout details as a json
+var options = <?php echo $json?>;
+
+ //document.razorpayform.submit();
+
+/**
+ * The entire list of Checkout fields is available at
+ * https://docs.razorpay.com/docs/checkout-form#checkout-fields
+ */
+options.handler = function (response){
+    document.getElementById('razorpay_payment_id').value = response.razorpay_payment_id;
+    document.getElementById('razorpay_signature').value = response.razorpay_signature;
+    document.razorpayform.submit();
+};
+
+// Boolean whether to show image inside a white frame. (default: true)
+options.theme.image_padding = false;
+
+options.modal = {
+    ondismiss: function() {
+        console.log("This code runs when the popup is closed");
+    },
+    // Boolean indicating whether pressing escape key 
+    // should close the checkout form. (default: true)
+    escape: true,
+    // Boolean indicating whether clicking translucent blank
+    // space outside checkout form should close the form. (default: false)
+    backdropclose: false
+};
+
+var rzp = new Razorpay(options);
+
+//document.getElementById('rzp-button1').onclick = function(e){
+    rzp.open();
+    e.preventDefault();
+//}
+</script>
